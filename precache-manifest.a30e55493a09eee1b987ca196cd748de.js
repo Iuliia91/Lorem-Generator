02a6dd5c8556b9fbf8056a6d508c27f0ad45674c
Iuliia91 <iuliiaprusakova91@gmail.com>
2022-11-05 13:59:10 +0200

    Updates

---
+++ precache-manifest.a30e55493a09eee1b987ca196cd748de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9544e7853916b23758968c6ca2b3afe",
+    "url": "/Lorem-Generator/index.html"
+  },
+  {
+    "revision": "37775337cdac87741914",
+    "url": "/Lorem-Generator/static/css/main.b06f8a47.chunk.css"
+  },
+  {
+    "revision": "c8f99de3883671709baa",
+    "url": "/Lorem-Generator/static/js/2.cad5d5ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lorem-Generator/static/js/2.cad5d5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37775337cdac87741914",
+    "url": "/Lorem-Generator/static/js/main.7c8b4b2c.chunk.js"
+  },
+  {
+    "revision": "a40c71d2d8766380229b",
+    "url": "/Lorem-Generator/static/js/runtime-main.eaf42075.js"
+  },
+  {
+    "revision": "e5ab169f319873d15f43fbf080015bf5",
+    "url": "/Lorem-Generator/static/media/close.e5ab169f.svg"
+  }
+]);
